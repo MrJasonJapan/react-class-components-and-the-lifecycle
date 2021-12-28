a0bc37d46b4e98128931f6c69bd876f18555729a
SpaGettys <jgettys@gmail.com>
2021-12-28 18:53:18 +0900

    Make sure the app can can still function even if the user denies sharing their location. Using conditional rendering, handle this with three scenarios: “Show latitude,” “Show error,” and “Show ‘loading’.”

--- src/index.js
+++ src/index.js
@@ -8,20 +8,30 @@ class App extends React.Component {
 
     // Initialize our state
     // THIS IS THE ONLY TIME we ever do direct assignment to this.state
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: "" };
 
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         // Update our state object
         this.setState({ lat: position.coords.latitude });
       },
-      (err) => console.log(err)
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
 
   // React requires that we define render()
   render() {
-    return <div>Latitude: {this.state.lat}</div>;
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading...</div>;
   }
 }
 ReactDOM.render(<App />, document.querySelector("#root"));
